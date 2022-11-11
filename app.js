d2fcfaab1b96761ec02fb3f74fe95b09ab5bc5e9
alexamox <alexamoks@yahoo.com>
2022-11-11 12:33:28 -0500

    chnange message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('welcome back god is good !'));
+app.get('/', (req, res) => res.send('always working hard !'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
