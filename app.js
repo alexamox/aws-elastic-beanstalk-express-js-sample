409c3d5425bdf8077048a41d8b4df5ee0437fef7
alexamox <alexamoks@yahoo.com>
2022-11-24 12:16:30 -0500

    change message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('still working hard !'));
+app.get('/', (req, res) => res.send('always gringing !'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
